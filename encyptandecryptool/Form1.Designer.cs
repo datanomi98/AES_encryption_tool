ed7efb45d3951586b0c87e6d07151fff2f04952c
datanomi <datanomi@protonmail.com>
2017-03-08 15:22:17 +0200

    AES salaus työkalu

---
+++ encyptandecryptool/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace encyptandecryptool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-3, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(369, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "decrypt file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(351, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "encrypt file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pass.";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 101);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(185, 105);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(203, 101);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "select and encrypt the folder";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(203, 130);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(157, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "select and decrypt the folder";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(592, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "If you want put password to folder encryption you have to put the password to the" +
+    " password field before you select the folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 240);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ encyptandecryptool/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace encyptandecryptool
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        public byte[] AES_Salaus(byte[] bytesencrypted, byte[] salasanabytes)
+        {
+            byte[] encyptedbytes = null;
+
+            //the salt bytes must be at least 8 bytes
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    var key = new Rfc2898DeriveBytes(salasanabytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesencrypted, 0, bytesencrypted.Length);
+                        cs.Close();
+
+                    }
+                    encyptedbytes = ms.ToArray();
+                }
+                return encyptedbytes;
+            }
+        }
+        public byte[] AES_salauksenpurku(byte[] bytesDecrypted, byte[] salasanabytes)
+        {
+
+            byte[] decryptedbytes = null;
+
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    var key = new Rfc2898DeriveBytes(salasanabytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesDecrypted, 0, bytesDecrypted.Length);
+                        cs.Close();
+
+                    }
+                    decryptedbytes = ms.ToArray();
+                }
+
+            }
+            return decryptedbytes;
+        }
+        public void EncryptFile(string inputFile, string outputFile)
+        {
+            try
+            {
+
+
+                //Password for the file
+                string salasana = textBox2.Text;
+
+                byte[] bytestoencrypted = File.ReadAllBytes(inputFile);
+                byte[] salasanabytes = Encoding.UTF8.GetBytes(salasana);
+
+                //hash the password with sha256
+                salasanabytes = SHA256.Create().ComputeHash(salasanabytes);
+
+                byte[] bytessalattu = AES_Salaus(bytestoencrypted, salasanabytes);
+
+                File.WriteAllBytes(outputFile, bytessalattu);
+                richTextBox1.Text = "salauksen teko onnistui\n encryption succeeded";
+
+            }
+            catch (Exception e)
+            {
+                richTextBox1.Text = "Jotain meni pileen\n something went wrong";
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog avaaTiedosto = new OpenFileDialog();
+
+            if (avaaTiedosto.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = avaaTiedosto.FileName;
+            }
+
+        }
+
+        private void DecryptFile(string inputFile, string outputFile)
+        {
+            try
+            {
+
+
+
+                string password = textBox2.Text;
+
+                byte[] bytesToBeDecrypted = File.ReadAllBytes(outputFile);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesDecrypted = AES_salauksenpurku(bytesToBeDecrypted, passwordBytes);
+
+
+                File.WriteAllBytes(outputFile, bytesDecrypted);
+                richTextBox1.Text = "salauksen purku onnistui\n Decryption succeeded";
+
+
+            }
+            catch (Exception e)
+            {
+                richTextBox1.Text = "Jotain meni pileen\n something went wrong";
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputpath = textBox1.Text;
+            string outputpath = inputpath;
+            EncryptFile(inputpath, outputpath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputpath = textBox1.Text;
+            string outputpath = inputpath;
+            DecryptFile(inputpath, outputpath);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        class globvar
+        {
+            public static string folder2;
+
+        }
+        public void button4_Click_1(object sender, EventArgs e)
+        {
+          
+        
+
+        
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string folder = folderBrowserDialog1.SelectedPath;
+            string[] kek = Directory.GetFileSystemEntries(folder);
+              
+                foreach (string kek2 in kek)
+                {
+
+                    globvar.folder2 = kek2;
+                    
+                         
+                }
+              
+            }
+    
+}
+      
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string folder = folderBrowserDialog1.SelectedPath;
+                string[] kek = Directory.GetFileSystemEntries(folder);
+
+                foreach (string kek2 in kek)
+                {
+
+                    EncryptFile(kek2, kek2);
+                  
+
+                }
+
+            }
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string folder = folderBrowserDialog1.SelectedPath;
+                string[] kek = Directory.GetFileSystemEntries(folder);
+
+                foreach (string kek2 in kek)
+                {
+
+                    DecryptFile(kek2, kek2);
+
+                }
+
+            }
+
+        }
+    }
+
+}
+   
+
+        
+
+       
+
+       
+    
+    
+     
+
